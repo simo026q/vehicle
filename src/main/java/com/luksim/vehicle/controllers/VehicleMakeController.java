841800585d1a1c73c603aad2857d09d3a8fe0ebd
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2024-02-21 14:45:02 +0100

    Added empty controllers

---
+++ src/main/java/com/luksim/vehicle/controllers/VehicleMakeController.java
@@ -0,0 +1,9 @@
+package com.luksim.vehicle.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vehicles/makes")
+public class VehicleMakeController {
+}
